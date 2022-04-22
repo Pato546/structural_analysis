1929a713e16a0c2be09662cb3c235b3685a746c7
Pato <boatengpato.pb@gmail.com>
2022-04-22 01:04:30 +0000

    Added some classes for Catching Errors

---
+++ src/structural_analysis/beam_errors.py
@@ -0,0 +1,10 @@
+class StaticallyUnstableExternally(Exception):
+    pass
+
+
+class StaticallyIndeterminateExternally(Exception):
+    pass
+
+
+class GeometricallyUnstableExternally(Exception):
+    pass
