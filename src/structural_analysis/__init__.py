7b8d7c728c68a5ff6719f131784799a9665aa8d2
Pato <boatengpato.pb@gmail.com>
2022-04-23 17:36:50 +0000

    Made some imports

--- src/structural_analysis/__init__.py
+++ src/structural_analysis/__init__.py
@@ -1,3 +1,10 @@
 from .load import PointLoad, UniformlyDistributedLoad, PointMoment
+from .beam import Beam
+from .beam_errors import (
+    GeometricallyUnstableExternally,
+    StaticallyUnstableExternally,
+    SupportCreationError,
+    StaticallyIndeterminateExternally,
+)
 
 __version__ = "1.0.0a"
