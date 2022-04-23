0ce296c4ae8cc47d22ecd4c4d500fb8ffcc77eaf
Pato <boatengpato.pb@gmail.com>
2022-04-23 23:39:44 +0000

    Statically Determinate Solver is working

--- src/main.py
+++ src/main.py
@@ -1,23 +1,34 @@
-from structural_analysis import PointLoad, UniformlyDistributedLoad
+from structural_analysis import PointLoad, UniformlyDistributedLoad, PointMoment
 from structural_analysis.statically_determinate.solver import (
     StaticallyDeterminateSolver,
 )
-from structural_analysis import Beam
-from structural_analysis.beam import create_support
+from structural_analysis import Beam, sign_convention
+from structural_analysis.beam import create_support, create_supports
 
 if __name__ == "__main__":
-    b = Beam(length=7)
-    s = create_support(rx=True, ry=True, rm=True)
-    udl = UniformlyDistributedLoad(-5, 5)
-    b.append_node('A', 0, 0, distributed_load=udl, support=s)
-    s2 = create_support(ry=True)
-    b.append_node('B', 5, 0, support=s2)
-    p = PointLoad(-10)
-    b.append_node('C', 7, 0, point_load=p)
+    sign_convention()
+    exit()
+    # Create Beam
+    b = Beam(length=8)
+
+    # Create Loads
+    # pl = PointLoad(-10)
+    udl = UniformlyDistributedLoad(-6, 3)
+    pm = PointMoment(-120)
+
+    # Create Supports
+    s1, *_ = create_supports([{'rx': True, 'ry': True, 'rm': True}])
+
+    b.append_node('A', 0, support=s1)
+    b.append_node('B', 2, point_moment=pm)
+    b.append_node('C', 4, distributed_load=udl)
+    b.append_node('D', 7)
+    b.append_node('E', 8)
 
     # print(b.get_total_support_reactions())
     # print(b.get_eqn_on_conditions())
     print(b.get_degree_of_ext_indeterminacy())
     print(b.classify_beam())
     solver = StaticallyDeterminateSolver(beam=b)
-    print(solver.total_rxn_generated_from_loads())
+    # print(solver.total_rxn_generated_from_loads())
+    print(solver.solve())
