ea97d6652f82d17e9ebaca0a4b25dce61d12c41b
Pato <boatengpato.pb@gmail.com>
2022-05-18 16:09:09 +0000

    minor testing

--- src/main.py
+++ src/main.py
@@ -5,69 +5,95 @@ from structural_analysis.statically_determinate.solver import (
 from structural_analysis import Beam
 from structural_analysis.beam import create_supports
 from structural_analysis.bending_shear import BendingShearCalculator
+from structural_analysis.statically_indeterminate.three_moment.solver import (
+    ThreeMomentSolver,
+)
 
 if __name__ == "__main__":
-    # p1 = PointLoad(40, angle_of_inclination=60)
-    # p2 = PointLoad(30, angle_of_inclination=25)
-    # p3 = p1 - p2
-    # print(p3.horizontal_force)
-    # print(p3.vertical_force)
-    # exit()
 
-    # Create Beam
-    b = Beam(length=14)
+    def display_results(solved_beam: Beam):
+        # TODO check RollerSupport direction
 
-    # Create Loads
-    pl1 = PointLoad(-160, angle_of_inclination=60)
-    # pl2 = PointLoad(-20)
-    udl1 = UniformlyDistributedLoad(-15, 6)
-    # udl2 = UniformlyDistributedLoad(-10, 3)
-    # udl3 = UniformlyDistributedLoad(-10, 1)
-    pm1 = PointMoment(400)
-    # pm2 = PointMoment(10)
+        for node in solved_beam:
+            if node.support:
+                if str(node.support) == "HingeSupport":
+                    if node.support.vertical_force < 0:
+                        print(
+                            f"{node.name}y = {abs(node.support.vertical_force):.2f} \u2193"
+                        )
+                    else:
+                        print(
+                            f"{node.name}y = {node.support.vertical_force:.2f} \u2191"
+                        )
 
-    # Create Supports
-    s1, s2, s3 = create_supports(
-        [{'ry': True, 'rx': True, 'rm': True}, {'ry': True, 'rx': True}, {'ry': True}])
-    s1.moment = -5.95
-    s1.vertical_force = 5.09
-    # s1.horizontal_force = 0
+                    if node.support.horizontal_force < 0:
+                        print(
+                            f"{node.name}x = {abs(node.support.horizontal_force):.2f} \u2190"
+                        )
+                    else:
+                        print(
+                            f"{node.name}x = {node.support.horizontal_force:.2f} \u2192"
+                        )
 
-    # s2.vertical_force = 25.815
-    # s3.vertical_force = 14.095
+                elif str(node.support) == "RollerSupport":
+                    if node.support.force < 0:
+                        print(f"{node.name}y = {abs(node.support.force):.2f} \u2193")
+                    else:
+                        print(f"{node.name}y = {node.support.force:.2f} \u2191")
 
-    b.append_node('A', 0, point_moment=pm1, distributed_load=udl1)
-    b.append_node('B', 6)
-    b.append_node('C', 10, point_load=pl1)
-    b.append_node('D', 14, support=s1)
-    # b.append_node('E', 12, point_load=pl2)
-    # b.append_node('F', 14, point_moment=pm1)
+                else:
+                    if node.support.moment < 0:
+                        print(f"{node.name}m = {abs(node.support.moment):.2f} \u21BA")
+                    else:
+                        print(f"{node.name}m = {node.support.moment:.2f} \u21BB")
 
-    # print(b.members)
-    # print(b.is_geometrically_stable())
-    # exit()
+                    if node.support.vertical_force < 0:
+                        print(
+                            f"{node.name}y = {abs(node.support.vertical_force):.2f} \u2193"
+                        )
+                    else:
+                        print(
+                            f"{node.name}y = {node.support.vertical_force:.2f} \u2191"
+                        )
 
-    # print(b.get_total_support_reactions())
-    # print(b.get_eqn_on_conditions())
-    # print(b.get_degree_of_ext_indeterminacy())
-    # print(b.classify_beam())
-    solver = StaticallyDeterminateSolver(beam=b)
-    # print(solver.total_rxn_generated_from_loads())
-    b_s = solver.solve()
-    for node in b_s:
-        if node.support:
-            if str(node.support) == 'HingeSupport':
-                print(node.support.vertical_force)
-                print(node.support.horizontal_force)
+                    if node.support.horizontal_force < 0:
+                        print(
+                            f"{node.name}x = {abs(node.support.horizontal_force):.2f} \u2190"
+                        )
+                    else:
+                        print(
+                            f"{node.name}x = {node.support.horizontal_force:.2f} \u2192"
+                        )
 
-            elif str(node.support) == 'RollerSupport':
-                print(node.support.force)
 
-            else:
-                print(node.support.moment)
-                print(node.support.vertical_force)
-                print(node.support.horizontal_force)
-    b_c = BendingShearCalculator(b_s)
-    print(b_c.calculate_bending())
-    print(b_c.calculate_shear())
-    # print(b_c.shear_force_equations)
+    b = Beam(8)
+    #
+    pl1 = PointLoad(-80)
+    # pl2 = PointLoad(-8)
+    # udl = UniformlyDistributedLoad(-3, 4)
+    #
+    s1, s2, s3, s4 = create_supports(
+        [
+            {"rx": True, "ry": True},
+            {"ry": True},
+            {"rx": True, "ry": True},
+            {"rx": True, "ry": True},
+        ]
+    )
+    #
+    # b.append_node('A', 0, support=s1)
+    # b.append_node('B', 2, point_load=pl1)
+    # b.append_node('C', 6, support=s2)
+    # b.append_node('D', 8, point_load=pl2)
+    # b.append_node('E', 11, support=s3, distributed_load=udl)
+    # b.append_node('F', 15, support=s4)
+    #
+    # m = ThreeMomentSolver(b)
+    # print(m.get_sub_beams())
+
+    b.append_node('A', 0, support=s1)
+    b.append_node('B', 4, point_load=pl1)
+    b.append_node('C', 8, support=s2)
+
+    b_s = StaticallyDeterminateSolver(b).solve()
+    display_results(b_s)
